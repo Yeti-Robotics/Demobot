787a6a6b6e218277c563e7852b5043de1d07bb7c
Antoine Campbell <acampbell@captechconsulting.com>
2015-01-17 14:14:27 -0500

    - cleaned up button binding method

--- src/org/usfirst/frc/team3506/robot/OI.java
+++ src/org/usfirst/frc/team3506/robot/OI.java
@@ -13,77 +13,69 @@ import org.usfirst.frc.team3506.robot.commands.TurnRightCommand;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
-	
-	
+	// // CREATING BUTTONS
+	// One type of button is a joystick button which is any button on a
+	// joystick.
+	// You create one by telling it which joystick it's on and which button
+	// number it is.
+	// Joystick stick = new Joystick(port);
+	// Button button = new JoystickButton(stick, buttonNumber);
+
+	// There are a few additional built in buttons you can use. Additionally,
+	// by subclassing Button you can create custom triggers and bind those to
+	// commands the same as any other Button.
+
+	// // TRIGGERING COMMANDS WITH BUTTONS
+	// Once you have a button, it's trivial to bind it to a button in one of
+	// three ways:
+
+	// Start the command when the button is pressed and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenPressed(new ExampleCommand());
+
+	// Run the command while the button is being held down and interrupt it once
+	// the button is released.
+	// button.whileHeld(new ExampleCommand());
+
+	// Start the command when the button is released and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenReleased(new ExampleCommand());
+
 	private Joystick leftJoy;
 	private Joystick rightJoy;
-	
+
 	public OI() {
 		leftJoy = new Joystick(LEFT_JOYSTICK);
 		rightJoy = new Joystick(RIGHT_JOYSTICK);
+
+		setJoystickButtonCommand(leftJoy, 5,	new TurnRightCommand());
+		setJoystickButtonCommand(leftJoy, 4, new TurnLeftCommand());
+		setJoystickButtonCommand(leftJoy, 3, new DriveStraightCommand());
+		setJoystickButtonCommand(leftJoy, 2, new TestCommandGroup());
+		setJoystickButtonCommand(leftJoy, 8, new RebootCommand());
 		
-		JoystickButton leftButton10 = new JoystickButton(leftJoy, 10);
-		leftButton10.whenPressed(new TurnRightCommand());
-		
-		JoystickButton leftButton7 = new JoystickButton(leftJoy, 7);
-		leftButton7.whenPressed(new TurnLeftCommand());
-		
-		JoystickButton leftButton3 = new JoystickButton(leftJoy, 3);
-		leftButton3.whenPressed(new DriveStraightCommand());
-		
-		JoystickButton leftButton2 = new JoystickButton(leftJoy, 2);
-		leftButton2.whenPressed(new TestCommandGroup());
-		
-		JoystickButton rightButton3 = new JoystickButton(rightJoy, 3);
-		rightButton3.whenPressed(new Piston1ForwardCommand());
-		
-		JoystickButton rightButton2 = new JoystickButton(rightJoy, 2);
-		rightButton2.whenPressed(new Piston1ReverseCommand());
-		
-		JoystickButton leftButton8 = new JoystickButton(leftJoy, 8);
-		leftButton8.whenPressed(new RebootCommand());
+		setJoystickButtonCommand(rightJoy, 3, new Piston1ForwardCommand());
+		setJoystickButtonCommand(rightJoy, 2, new Piston1ReverseCommand());
 	}
-	
+
 	public Joystick getLeftJoy() {
 		return leftJoy;
 	}
-	
+
 	public Joystick getRightJoy() {
 		return rightJoy;
 	}
-	
-	
-}
 
+	private void setJoystickButtonCommand(Joystick joystick, int button,
+			Command command) {
+		new JoystickButton(joystick, button).whenPressed(command);
+	}
+
+}
