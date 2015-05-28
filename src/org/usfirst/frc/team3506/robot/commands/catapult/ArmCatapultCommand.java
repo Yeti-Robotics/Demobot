85f0ae5b7027cb840db554d7d9c5a5ba1fed115c
Sam Perlmutter <sam.perlmutter227@gmail.com>
2015-05-28 10:23:42 -0400

    Update ArmCatapultCommand.java

--- src/org/usfirst/frc/team3506/robot/commands/catapult/ArmCatapultCommand.java
+++ src/org/usfirst/frc/team3506/robot/commands/catapult/ArmCatapultCommand.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team3506.robot.commands.catapult;
 
+import org.usfirst.frc.team3506.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -8,8 +10,7 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ArmCatapultCommand extends Command {
 
     public ArmCatapultCommand() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        requires(Robot.catapultSubsystem);
     }
 
     // Called just before this Command runs the first time
@@ -18,6 +19,7 @@ public class ArmCatapultCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.catapultSubsystem.extendPiston();
     }
 
     // Make this return true when this Command no longer needs to run execute()
