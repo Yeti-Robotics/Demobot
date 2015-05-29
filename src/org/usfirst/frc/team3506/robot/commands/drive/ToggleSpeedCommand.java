3ce4a969501bf23c116f8a78cf61a2884cd41f2b
wazateer1 <wazateer1@gmail.com>
2015-05-29 09:30:58 -0400

    - We want to be absolutely sure this only runs once

--- src/org/usfirst/frc/team3506/robot/commands/drive/ToggleSpeedCommand.java
+++ src/org/usfirst/frc/team3506/robot/commands/drive/ToggleSpeedCommand.java
@@ -16,11 +16,11 @@ public class ToggleSpeedCommand extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        Robot.safeSpeed = !Robot.safeSpeed;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.safeSpeed = !Robot.safeSpeed;
     }
 
     // Make this return true when this Command no longer needs to run execute()
